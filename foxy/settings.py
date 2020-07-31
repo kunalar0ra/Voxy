4094524990535c8940d8169f41bdfbb694b3849c
kunalar0ra <65423827+kunalar0ra@users.noreply.github.com>
2020-07-31 09:47:47 +0530

    Updated settings.py

--- foxy/settings.py
+++ foxy/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c2ak-wqs13cf#s5jvxw8rd-zbt@a$_wd@ib-^-!_l69eo*ka$3'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
